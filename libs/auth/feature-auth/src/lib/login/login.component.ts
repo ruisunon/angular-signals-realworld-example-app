82f8150afd3ee464cd372c42eb1c97f1853111a6
stefanos- <stefanoslignos@gmail.com>
2023-07-02 15:16:33 +0200

    Fix build issues

--- libs/auth/feature-auth/src/lib/login/login.component.ts
+++ libs/auth/feature-auth/src/lib/login/login.component.ts
@@ -1,55 +1,14 @@
-import { DynamicFormComponent, Field, formsActions, ListErrorsComponent, ngrxFormsQuery } from '@realworld/core/forms';
-import { ChangeDetectionStrategy, Component, OnDestroy, OnInit } from '@angular/core';
-import { Validators } from '@angular/forms';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterModule } from '@angular/router';
-import { authActions } from '@realworld/auth/data-access';
-import { Store } from '@ngrx/store';
-
-const structure: Field[] = [
-  {
-    type: 'INPUT',
-    name: 'email',
-    placeholder: 'Username',
-    validator: [Validators.required],
-  },
-  {
-    type: 'INPUT',
-    name: 'password',
-    placeholder: 'Password',
-    validator: [Validators.required],
-    attrs: {
-      type: 'password',
-    },
-  },
-];
 
 @Component({
-  selector: 'cdt-login',
   standalone: true,
+  selector: 'sra-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css'],
-  imports: [ListErrorsComponent, DynamicFormComponent, RouterModule],
+  imports: [FormsModule, RouterModule],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class LoginComponent implements OnInit, OnDestroy {
-  structure$ = this.store.select(ngrxFormsQuery.selectStructure);
-  data$ = this.store.select(ngrxFormsQuery.selectData);
-
-  constructor(private readonly store: Store) {}
-
-  ngOnInit() {
-    this.store.dispatch(formsActions.setStructure({ structure }));
-  }
-
-  updateForm(changes: any) {
-    this.store.dispatch(formsActions.updateData({ data: changes }));
-  }
-
-  submit() {
-    this.store.dispatch(authActions.login());
-  }
-
-  ngOnDestroy() {
-    this.store.dispatch(formsActions.initializeForm());
-  }
+export class LoginComponent {
+  submit() {}
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -3,8 +3,12 @@ import { Route } from '@angular/router';
 export const appRoutes: Route[] = [
   {
     path: 'login',
+    loadComponent: () =>
+      import('@auth/feature-auth').then((f) => f.LoginComponent),
   },
   {
     path: 'register',
+    loadComponent: () =>
+      import('@auth/feature-auth').then((f) => f.RegisterComponent),
   },
 ];
